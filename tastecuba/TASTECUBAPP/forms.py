c4fb838f1b8e8674b4acc61ec28dfdc6e346576e
Jacob Lynch <jclynch4@asu.edu>
2021-06-26 11:01:00 -0700

    clean up

--- tastecuba/TASTECUBAPP/forms.py
+++ tastecuba/TASTECUBAPP/forms.py
@@ -7,16 +7,14 @@ from django.contrib.auth.models import User
 
 class NewUserForm(UserCreationForm):
     email = forms.EmailField(required=True)
-    other = forms.Other(required=True)
 
     class Meta:
         model = User
-        fields = ("username", "email", "password1", "password2", "other",)
+        fields = ("username", "email", "password1", "password2",)
 
     def save(self, commit=True):
         user = super(NewUserForm, self).save(commit=False)
         user.email = self.cleaned_data['email']
-        user.other = self.cleaned_date['other']
         if commit:
             user.save()
         return user
